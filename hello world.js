6532f504dfe34dcd30ef265ace63b754aaf6dde1
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-03-01 20:15:07 +0530

    hello world using js
    hello world using js

---
+++ hello world.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello world</title>
+</head>
+<body>
+
+	<script type="text/javascript" language="javascript">
+		document.write("hellooo");
+	</script>
+
+</body>
+</html>
